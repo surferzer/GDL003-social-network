0bad79bc0b33b1c574555f5d2285de7373e3cc95
unknown <leslalazc@gmail.com>
2019-07-17 17:58:14 -0500

    se agrega campo funcional de Check in con correo

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_kJkQ9LzVm9XJz-TA9jHMuH-yC367zIU",
+  authDomain: "garnacha-love-rs.firebaseapp.com",
+  databaseURL: "https://garnacha-love-rs.firebaseio.com",
+  projectId: "garnacha-love-rs",
+  storageBucket: "",
+  messagingSenderId: "671471595388",
+  appId: "1:671471595388:web:19cc42fa4d01f3a3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function registrar(){
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    
+    console.log(email);
+    console.log(password);
+
+ firebase.auth().createUserWithEmailAndPassword(email, password)
+        .catch(function(error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage) ;
+        // ...
+      });
+     
+}
+
+
+
+
+  
