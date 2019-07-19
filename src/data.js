2473c152aafab3180df22c8431db9a8fb1d139fa
unknown <leslalazc@gmail.com>
2019-07-19 17:17:50 -0500

    se agregan campos de autenticacion funcionales con farebase-ui

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+//se agregan las configuraciones de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC_kJkQ9LzVm9XJz-TA9jHMuH-yC367zIU",
   authDomain: "garnacha-love-rs.firebaseapp.com",
@@ -16,7 +17,7 @@ function registrar(){
     
     console.log(email);
     console.log(password);
-
+// toma el valor de email y password y al no autenticarlo imprime error 
  firebase.auth().createUserWithEmailAndPassword(email, password)
         .catch(function(error) {
         // Handle Errors here.
@@ -29,8 +30,37 @@ function registrar(){
       });
      
 }
+let ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+let uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // 
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: 'index.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: 'index.html',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
 
-
-
-  
+ui.start('#firebaseui-auth-container', uiConfig);
