c809bed1e022db315c8c0e9b15e196c08fb90b32
surferzer <mafernandazr@gmail.com>
2019-08-14 16:28:01 -0500

    cambios finales de muro y likes

--- src/likes.js
+++ src/likes.js
@@ -1,9 +1,32 @@
 //function contador de likes
 var i = 0;
-function contador()
-{
-i = i + 1;
-var btn = document.getElementById("boton");
-btn.value = "" + i + "";
-}
+function contador(){
+    if(i+= 1){
+
+    //var btn = document.getElementById("boton");
+    console.log( btn.value = "" + i + "");
+    }
+    else{
+        
+    }
+
+//i = i + 1;
+
+//console.log(contador);
+};
+
+document.getElementById("botonLike").addEventListener("click", contador);
+
+
+
+function cambiar(){
+    if (document.getElementById('botonLike').src == "https://cdn4.iconfinder.com/data/icons/like-18/32/459-01-512.png")
+    {
+        document.getElementById('botonLike').src = "https://yokoent.com/images/thumbs-up-png-red-1.png";
+    }
+    else
+    {
+        document.getElementById('botonLike').src = "https://cdn4.iconfinder.com/data/icons/like-18/32/459-01-512.png";
+    }
+    };
 
